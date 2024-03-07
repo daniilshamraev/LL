5f163614d363676d98f0eb8706aa0791c8f70a30
Daniil Shamraev <46665117+daniilshamraev@users.noreply.github.com>
2024-03-07 12:51:03 +0300

    del border from place

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -43,30 +43,133 @@ function Games() {
             <div className="flex flex-col pt-8">
                 {
                     typeof games === "boolean" && (
-                        <div
-                            className="shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                        <>
                             <div
-                                className="animate-pulse flex space-x-4">
+                                className="shadow rounded-md p-4 max-w-sm w-full mx-auto">
                                 <div
-                                    className="rounded-full bg-slate-700 h-20 w-20"></div>
-                                <div
-                                    className="flex-1 space-y-6 py-1">
+                                    className="animate-pulse flex space-x-4">
                                     <div
-                                        className="h-2 bg-slate-700 rounded"></div>
-                                    <div className="space-y-3">
-                                        <div
-                                            className="grid grid-cols-3 gap-4">
-                                            <div
-                                                className="h-2 bg-slate-700 rounded col-span-2"></div>
-                                            <div
-                                                className="h-2 bg-slate-700 rounded col-span-1"></div>
-                                        </div>
+                                        className="rounded-full bg-slate-700 h-20 w-20"></div>
+                                    <div
+                                        className="flex-1 space-y-6 py-1">
                                         <div
                                             className="h-2 bg-slate-700 rounded"></div>
+                                        <div
+                                            className="space-y-3">
+                                            <div
+                                                className="grid grid-cols-3 gap-4">
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-2"></div>
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-1"></div>
+                                            </div>
+                                            <div
+                                                className="h-2 bg-slate-700 rounded"></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                            <div
+                                className="shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                                <div
+                                    className="animate-pulse flex space-x-4">
+                                    <div
+                                        className="rounded-full bg-slate-700 h-20 w-20"></div>
+                                    <div
+                                        className="flex-1 space-y-6 py-1">
+                                        <div
+                                            className="h-2 bg-slate-700 rounded"></div>
+                                        <div
+                                            className="space-y-3">
+                                            <div
+                                                className="grid grid-cols-3 gap-4">
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-2"></div>
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-1"></div>
+                                            </div>
+                                            <div
+                                                className="h-2 bg-slate-700 rounded"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className="shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                                <div
+                                    className="animate-pulse flex space-x-4">
+                                    <div
+                                        className="rounded-full bg-slate-700 h-20 w-20"></div>
+                                    <div
+                                        className="flex-1 space-y-6 py-1">
+                                        <div
+                                            className="h-2 bg-slate-700 rounded"></div>
+                                        <div
+                                            className="space-y-3">
+                                            <div
+                                                className="grid grid-cols-3 gap-4">
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-2"></div>
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-1"></div>
+                                            </div>
+                                            <div
+                                                className="h-2 bg-slate-700 rounded"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className="shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                                <div
+                                    className="animate-pulse flex space-x-4">
+                                    <div
+                                        className="rounded-full bg-slate-700 h-20 w-20"></div>
+                                    <div
+                                        className="flex-1 space-y-6 py-1">
+                                        <div
+                                            className="h-2 bg-slate-700 rounded"></div>
+                                        <div
+                                            className="space-y-3">
+                                            <div
+                                                className="grid grid-cols-3 gap-4">
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-2"></div>
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-1"></div>
+                                            </div>
+                                            <div
+                                                className="h-2 bg-slate-700 rounded"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div
+                                className="shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                                <div
+                                    className="animate-pulse flex space-x-4">
+                                    <div
+                                        className="rounded-full bg-slate-700 h-20 w-20"></div>
+                                    <div
+                                        className="flex-1 space-y-6 py-1">
+                                        <div
+                                            className="h-2 bg-slate-700 rounded"></div>
+                                        <div
+                                            className="space-y-3">
+                                            <div
+                                                className="grid grid-cols-3 gap-4">
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-2"></div>
+                                                <div
+                                                    className="h-2 bg-slate-700 rounded col-span-1"></div>
+                                            </div>
+                                            <div
+                                                className="h-2 bg-slate-700 rounded"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
                     )
                 }
                 {
